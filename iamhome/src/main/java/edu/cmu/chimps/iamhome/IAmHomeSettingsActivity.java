1ae83d70a766739a4853932dc7d7bf0a5d095b63
YDTQHD <linhancheng14@icloud.com>
2017-07-20 17:55:58 -0400

    Merge

--- iamhome/src/main/java/edu/cmu/chimps/iamhome/IAmHomeSettingsActivity.java
+++ iamhome/src/main/java/edu/cmu/chimps/iamhome/IAmHomeSettingsActivity.java
@@ -1,18 +1,63 @@
 package edu.cmu.chimps.iamhome;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 
-public class IAmHomeSettingsActivity extends Activity {
+import static android.app.PendingIntent.getService;
 
+public class IAmHomeSettingsActivity extends Activity implements View.OnClickListener {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sendNotice = (Button) findViewById(R.id.button_notice);
+        Button whatsApp = (Button) findViewById(R.id.button_WhatsApp);
+        sendNotice.setOnClickListener(this);
+        whatsApp.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+
+        String[] contactNames = {"Edwin"};
+        Intent launchService = new Intent(this, ShareMessageService.class);
+        launchService.putExtra("contactNames", contactNames);
+
+        switch (v.getId()) {
+
+            case R.id.button_notice:
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new NotificationCompat.Builder(this)
+                        .setContentTitle("You just arrived home!")
+                        .setContentText("Notify your contacts now!")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setPriority(Notification.PRIORITY_MAX) //为什么不work????
+                        .setContentIntent(getService(this, 0, launchService, 0))
+                        .setAutoCancel(true)
+                        .build();
+                manager.notify(1, notification);
+                break;
+
+            case R.id.button_WhatsApp:
+                startService(launchService);
+                break;
+
+            default:
+                break;
+        }
+    }
 }
---
+++ iamhome/src/main/java/edu/cmu/chimps/iamhome/ShareMessageService.java
@@ -0,0 +1,99 @@
+package edu.cmu.chimps.iamhome;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.github.privacystreams.accessibility.AccEvent;
+import com.github.privacystreams.core.Callback;
+import com.github.privacystreams.core.Item;
+import com.github.privacystreams.core.UQI;
+import com.github.privacystreams.core.purposes.Purpose;
+import com.github.privacystreams.utils.AppUtils;
+
+import edu.cmu.chimps.iamhome.utils.AutoSelectUtils;
+
+public class ShareMessageService extends Service {
+
+    UQI uqi;
+    String[] contactNames;
+    Boolean clicked;
+    private static final String TERM_ARRIVED_HOME = "Hey, I just arrived my house!";
+    private static final String CONTACT_RESOURCE_ID = "contactpicker_row_name";
+
+    private NodeInfoListener nodeInfoListener;
+
+    public void setNodeInfoListener(NodeInfoListener nodeInfoListener) {
+        this.nodeInfoListener = nodeInfoListener;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        uqi = new UQI(this);
+        clicked = false;
+        contactNames = intent.getStringArrayExtra("contactNames");
+        AutoSelectUtils autoSelectUtils = new AutoSelectUtils();
+
+        setNodeInfoListener(new NodeInfoListener() {
+            public void nodeInfoReceived(AccessibilityNodeInfo selectingView) {
+                Log.e("HI", "NodeINfoReceived");
+                if (!clicked) {
+                    Log.e("List Size", Integer.toString(contactNames.length));
+                    clicked = AutoSelectUtils.autoSelect(contactNames, selectingView);
+                }
+                stopSelf();
+            }
+        });
+
+        uqi.getData(AccEvent.asUpdates(), Purpose.FEATURE("base event"))
+                .forEach(new Callback<Item>() {
+                    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+                    @Override
+
+                    protected void onInput(Item item) {
+                        AccessibilityNodeInfo root = item.getValueByField(AccEvent.ROOT_NODE);
+                        if (root != null && root.getPackageName().equals(AppUtils.APP_PACKAGE_WHATSAPP)
+                                && ((int) item.getValueByField(AccEvent.EVENT_TYPE) == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED)) {
+                            nodeInfoListener.nodeInfoReceived(root);
+                            Log.e("UQI Thread", "Transferred");
+                        }
+                    }
+                });
+
+        autoSelectUtils.autoLaunch(this, TERM_ARRIVED_HOME, AppUtils.APP_PACKAGE_WHATSAPP);
+
+        return START_NOT_STICKY;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        try {
+            Log.e("stopped", "stopped");
+            uqi.stopAll();
+        } catch (Exception e) {
+            Log.e("stopped", "exception");
+            e.printStackTrace();
+        }
+        uqi = null;
+        clicked = false;
+        super.onDestroy();
+    }
+}
