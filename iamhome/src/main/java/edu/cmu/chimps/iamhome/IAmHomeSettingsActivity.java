63feb898fa8f184bdf878976cde5bf0c340f4b44
JiaYeMao <610275388@qq.com>
2017-07-21 11:45:52 -0400

    add something
    items are selected from beginning, accroding to savedContactList

--- iamhome/src/main/java/edu/cmu/chimps/iamhome/IAmHomeSettingsActivity.java
+++ iamhome/src/main/java/edu/cmu/chimps/iamhome/IAmHomeSettingsActivity.java
@@ -1,35 +1,33 @@
 package edu.cmu.chimps.iamhome;
 
-import android.app.Activity;
-import android.content.Context;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
+
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import edu.cmu.chimps.iamhome.RecyView.Contact;
 import edu.cmu.chimps.iamhome.RecyView.ContactAdapter;
-import edu.cmu.chimps.iamhome.utils.AlarmUtils;
-import edu.cmu.chimps.iamhome.utils.WifiUtils;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.NotificationCompat;
-import android.widget.Button;
+import edu.cmu.chimps.iamhome.RecyView.ContactStorage;
+
 import static android.app.PendingIntent.getService;
 
 public class IAmHomeSettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView textView;
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +36,12 @@ public class IAmHomeSettingsActivity extends AppCompatActivity implements View.O
         setTitle("Select Contacts");
         //initialize contactlist from whatsapp
         Contact.contactList = Contact.getWhatsAppContacts(this);
+        ContactStorage.InitSelection(this);
         ContactAdapter adapter = new ContactAdapter(Contact.contactList, IAmHomeSettingsActivity.this);
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-        textView = (TextView)findViewById(R.id.textview);
         //set the alarm
 //        AlarmUtils.setAlarm(this, 14,20,00);
 //        startService(new Intent(this, IAmHomePlugin.class));
@@ -53,7 +51,6 @@ public class IAmHomeSettingsActivity extends AppCompatActivity implements View.O
         Button whatsApp = (Button) findViewById(R.id.button_WhatsApp);
         sendNotice.setOnClickListener(this);
         whatsApp.setOnClickListener(this);
-
 
     }
 
@@ -71,14 +68,15 @@ public class IAmHomeSettingsActivity extends AppCompatActivity implements View.O
                 savedContactList.add(Contact.contactList.get(i).getName());
             }
         }
-        Toast.makeText(this, "Contacts saved" , Toast.LENGTH_SHORT).show();
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < savedContactList.size(); i++){
-            builder.append(savedContactList.get(i));
-        }
-        textView.setText(builder);
+        //Toast.makeText(this, "Contacts saved" , Toast.LENGTH_SHORT).show();
+
+
+        Set<String> set = new HashSet<>(savedContactList);
+        ContactStorage.storeSendUsers(this, set);
+
         return true;
     }
+
 
 
     public void onClick(View v) {
--- iamhome/src/main/java/edu/cmu/chimps/iamhome/RecyView/ContactAdapter.java
+++ iamhome/src/main/java/edu/cmu/chimps/iamhome/RecyView/ContactAdapter.java
@@ -1,17 +1,13 @@
 package edu.cmu.chimps.iamhome.RecyView;
 
 import android.app.Activity;
-import android.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -24,9 +20,7 @@ import edu.cmu.chimps.iamhome.R;
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
     private List<Contact> mContactList;
     private Activity mActivity;
-    private ActionMode mActionmode;
 
-    //新建viewholder类
     static class ViewHolder extends RecyclerView.ViewHolder{
         View contactView;
         ImageView contactImage;
@@ -41,29 +35,6 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
             contactName = itemView.findViewById(R.id.contact_name);
         }
     }
-    //创建 ActionMode.Callback实例
-    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback(){
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            mode.getMenuInflater().inflate(R.menu.actionmode, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return false;
-        }
-
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            return false;
-        }
-
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-
-        }
-    };
 
 
     public ContactAdapter(List<Contact> mContactList, Activity activity) {
@@ -82,17 +53,16 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
                 int position = holder.getAdapterPosition();
                 Contact contact = mContactList.get(position);
                 if (SelectedItemCount()==0){
-                    //mActionmode =  mActivity.startActionMode(mActionModeCallback);
-                    toggleSelection(holder, contact);
+                    toggleFlag(contact);
                     String title = " " + SelectedItemCount() + "selected";
                     mActivity.setTitle(title);
                 } else {
-                    toggleSelection(holder, contact);
+                    toggleFlag(contact);
                     String title = " " + SelectedItemCount() + " selected";
                     mActivity.setTitle(title);
                 }
-                Toast.makeText(view.getContext(), "click " + "position:"+position, Toast.LENGTH_SHORT).show();
-
+                SetSelection(holder, contact);
+                //Toast.makeText(view.getContext(), "click " + "position:"+position, Toast.LENGTH_SHORT).show();
             }
         });
         return holder;
@@ -103,11 +73,7 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         Contact contact = mContactList.get(position);
         holder.contactImage.setImageDrawable(contact.getContactPicture());
         holder.contactName.setText(contact.getName());
-        if (contact.isFlag()){
-            holder.contactLayout.setSelected(true);
-        }else {
-            holder.contactLayout.setSelected(false);
-        }
+        SetSelection(holder, contact);
     }
 
     @Override
@@ -125,13 +91,20 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         return count;
     }
 
-    public void toggleSelection(ViewHolder holder, Contact contact){
+    public void toggleFlag(Contact contact){
         if (contact.isFlag()){
             contact.setFlag(false);
-            holder.contactLayout.setSelected(false);
         } else {
             contact.setFlag(true);
-            holder.contactLayout.setSelected(true);
         }
     }
+
+    public  void SetSelection(ViewHolder holder, Contact contact){
+        if (contact.isFlag()){
+            holder.contactLayout.setSelected(true);
+        }else {
+            holder.contactLayout.setSelected(false);
+        }
+    }
+
 }
