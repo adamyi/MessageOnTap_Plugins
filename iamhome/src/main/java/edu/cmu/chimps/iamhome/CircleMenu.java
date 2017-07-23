b5213bb7f53c844217d9bfa42729469352f3d79a
everbrightw <yusencs@163.com>
2017-07-23 16:31:34 -0400

    add wel_come page

---
+++ iamhome/src/main/java/edu/cmu/chimps/iamhome/CircleMenu.java
@@ -0,0 +1,10 @@
+package edu.cmu.chimps.iamhome;
+
+/**
+ * Created by wangyusen on 7/23/17.
+ */
+
+public class CircleMenu {
+
+
+}
---
+++ iamhome/src/main/java/edu/cmu/chimps/iamhome/ListActivity.java
@@ -0,0 +1,21 @@
+package edu.cmu.chimps.iamhome;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by wangyusen on 7/23/17.
+ */
+
+public class ListActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
