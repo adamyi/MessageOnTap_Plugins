db3bc762752be21373080be591ef92ee6f6591b7
JiaYeMao <610275388@qq.com>
2017-07-28 17:37:06 -0400

    Merge branch 'master' of https://github.com/adamyi/MessageOnTap_Plugins

--- smart-calendar/src/main/java/edu/cmu/chimps/smart_calendar/SmartCalendarSettingsActivity.java
+++ smart-calendar/src/main/java/edu/cmu/chimps/smart_calendar/SmartCalendarSettingsActivity.java
@@ -1,8 +1,12 @@
 package edu.cmu.chimps.smart_calendar;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 
 public class SmartCalendarSettingsActivity extends PreferenceActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
