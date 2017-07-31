36e82d09dec81fa76a004731a52168119dc74de3
JiaYeMao <610275388@qq.com>
2017-07-31 15:35:09 -0400

    ++

--- googledocsplugin/src/main/java/edu/cmu/chimps/googledocsplugin/GoogleDocsPlugin.java
+++ googledocsplugin/src/main/java/edu/cmu/chimps/googledocsplugin/GoogleDocsPlugin.java
@@ -1,23 +1,21 @@
 package edu.cmu.chimps.googledocsplugin;
 
-import android.os.RemoteException;
 import android.util.Log;
-
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import edu.cmu.chimps.messageontap_api.DataUtils;
-import edu.cmu.chimps.messageontap_api.EntityAttributes;
-import edu.cmu.chimps.messageontap_api.Globals;
+import edu.cmu.chimps.messageontap_api.JSONUtils;
 import edu.cmu.chimps.messageontap_api.MessageOnTapPlugin;
 import edu.cmu.chimps.messageontap_api.MethodConstants;
+import edu.cmu.chimps.messageontap_api.ParseTree;
 import edu.cmu.chimps.messageontap_api.PluginData;
 import edu.cmu.chimps.messageontap_api.Tag;
 import edu.cmu.chimps.messageontap_api.Trigger;
-
-import edu.cmu.chimps.messageontap_api.MessageData;
 
 
 
@@ -103,27 +101,24 @@ public class GoogleDocsPlugin extends MessageOnTapPlugin {
     @Override
     protected void initNewSession(long sid, HashMap<String, Object> params) throws Exception {
         Log.e(TAG, "Session created here!");
-        Log.e(TAG, DataUtils.hashMapToString(params));
+        Log.e(TAG, JSONUtils.hashMapToString(params));
         // TID is something we might need to implement stateflow inside a plugin.
-
-        DocList = new ArrayList<>();
-        params.put("FindDoc", DocList);
 
         //todo: if root is not googleDoc, add it
         //能不能找GoogleDoc？
         if (triggerListHasName.contains(params.get(Session.TRIGGER_SOURCE))){
             treeForSearch = new ParseTree();                              //Initial a new tree, only has one node
-            Node newNode = new Node();
+            ParseTree.Node newNode = new ParseTree.Node();
             treeForSearch.setNodeById(GOOGLEDOC_URL, newNode);
             tree1 = params.get(Graph.SYNTAX_TREE);
             params.remove(Graph.SYNTAX_TREE);
             params.put(Graph.SYNTAX_TREE, tree1);
-            TidFindAllDoc = newTaskResponsed(sid, MethodConstants.PKG, MethodConstants.GRAPH_RETRIEVAL, params);
+            TidFindAllDoc = newTaskResponsed(sid, MethodConstants.PERSONAL_GRAPE_TYPE, MethodConstants.GRAPH_RETRIEVAL, params);
         } else {
             tree2 = params.get(Graph.SYNTAX_TREE);
             tree2 = AddRoot(tree2);
             params.put(Graph.SYNTAX_TREE, tree2);
-            TidFindDoc = newTaskResponsed(sid, MethodConstants.PKG, MethodConstants.GRAPH_RETRIEVAL, params);
+            TidFindDoc = newTaskResponsed(sid, MethodConstants.PERSONAL_GRAPE_TYPE, MethodConstants.GRAPH_RETRIEVAL, params);
         }
 
     }
@@ -131,7 +126,7 @@ public class GoogleDocsPlugin extends MessageOnTapPlugin {
     @Override
     protected void newTaskResponsed(long sid, long tid, HashMap<String, Object> params) throws Exception {
         Log.e(TAG, "Got task response!");
-        Log.e(TAG, DataUtils.hashMapToString(params));
+        Log.e(TAG, JSONUtils.hashMapToString(params));
 
         ArrayList<Doc> DocList = new ArrayList<>();
         if (tid == TidFindAllDoc) {
@@ -197,19 +192,20 @@ public class GoogleDocsPlugin extends MessageOnTapPlugin {
             Log.e(TAG, "Session ended");
         }
 
-
     }
 
 
     private ParseTree AddRoot(ParseTree tree){
-        for (int i; i<tree.){
-            if (node.getParent() == 0){
-                node.setParent(ROOT);
-                Node newNode = new Node();
-                newNode.setId(Event Name);
-                newNode.setParent(0);
-                newNode.setChildren(node.getId());
-                node.addTag(Graph.Document.URL);
+        for (ParseTree.Node node : tree.Node){
+            if (node.getParentId() == 0){
+                node.setParentId(213123);
+                ParseTree.Node newNode = new ParseTree.Node();
+                newNode.setId(23434324);
+                newNode.setParentId(0);
+                Set<Integer> set = new HashSet<>();
+                set.add(node.getId());
+                newNode.setChildrenIds(set);
+                newNode.addTag(Graph.Document.URL);
             }
         }
         return tree;
--- smart-calendar/src/main/java/edu/cmu/chimps/smart_calendar/SmartCalendarPlugin.java
+++ smart-calendar/src/main/java/edu/cmu/chimps/smart_calendar/SmartCalendarPlugin.java
@@ -3,7 +3,6 @@ package edu.cmu.chimps.smart_calendar;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashMap;
@@ -11,10 +10,12 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 import edu.cmu.chimps.messageontap_api.DataUtils;
+import edu.cmu.chimps.messageontap_api.JSONUtils;
 import edu.cmu.chimps.messageontap_api.MessageOnTapPlugin;
 import edu.cmu.chimps.messageontap_api.MethodConstants;
 import edu.cmu.chimps.messageontap_api.ParseTree;
 import edu.cmu.chimps.messageontap_api.PluginData;
+import edu.cmu.chimps.messageontap_api.Session;
 import edu.cmu.chimps.messageontap_api.Tag;
 import edu.cmu.chimps.messageontap_api.Trigger;
 
@@ -108,12 +109,12 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
     @Override
     protected void initNewSession(long sid, HashMap<String, Object> params) throws Exception {
         Log.e(TAG, "Session created here!");
-
+        Log.e(TAG, JSONUtils.hashMapToString(params));
         // TID is something we might need to implement stateflow inside a plugin.
 
         if (Session.TRIGGER_SOURCE.equals("Trigger name")){
             tree1 = (ParseTree)params.get(Graph.SYNTAX_TREE);
-            Node EventTime = new Node();
+            ParseTree.Node EventTime = new ParseTree.Node();
             Node EventName = new Node();
             EventTime.setId(Root_ID);
             EventName.setId(Node_ID);
@@ -126,7 +127,7 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
             //Retrieval events
             params.put(Graph.SYNTAX_TREE, tree1);
 
-            TidShow1 = newTaskRequest(sid, MethodConstants.PKG, MethodConstants.GRAPH_RETRIEVAL, params);
+            TidShow1 = newTaskRequest(sid, MethodConstants.PERSONAL_GRAPE_TYPE, MethodConstants.GRAPH_RETRIEVAL, params);
         }
 
         if (Session.TRIGGER_SOURCE.equals("Trigger name2")){
@@ -141,7 +142,7 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
     @Override
     protected void newTaskResponsed(long sid, long tid, HashMap<String, Object> params) throws Exception {
         Log.e(TAG, "Got task response!");
-        Log.e(TAG, DataUtils.hashMapToString(params));
+        Log.e(TAG, JSONUtils.hashMapToString(params));
 
         ArrayList<ArrayList<Object>> eventList;
             if (tid == TidShow1) {
