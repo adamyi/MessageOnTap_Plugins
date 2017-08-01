93235480a55d2a9cc212255f3015343d006c2224
JiaYeMao <610275388@qq.com>
2017-08-01 12:05:08 -0400

    add date transfer

--- smart-calendar/src/main/java/edu/cmu/chimps/smart_calendar/SmartCalendarPlugin.java
+++ smart-calendar/src/main/java/edu/cmu/chimps/smart_calendar/SmartCalendarPlugin.java
@@ -8,11 +8,9 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
-import edu.cmu.chimps.messageontap_api.DataUtils;
-import edu.cmu.chimps.messageontap_api.EntityAttributes;
 import edu.cmu.chimps.messageontap_api.JSONUtils;
 import edu.cmu.chimps.messageontap_api.MessageOnTapPlugin;
 import edu.cmu.chimps.messageontap_api.MethodConstants;
@@ -24,7 +22,11 @@ import edu.cmu.chimps.messageontap_api.Tag;
 import edu.cmu.chimps.messageontap_api.Trigger;
 
 import static edu.cmu.chimps.messageontap_api.ParseTree.Direction;
-import static edu.cmu.chimps.messageontap_api.ParseTree.Node;
+import static edu.cmu.chimps.smart_calendar.StringUtils.DAY;
+import static edu.cmu.chimps.smart_calendar.StringUtils.LOCATIONROOTID;
+import static edu.cmu.chimps.smart_calendar.StringUtils.MONTH;
+import static edu.cmu.chimps.smart_calendar.StringUtils.NAMEROOTID;
+import static edu.cmu.chimps.smart_calendar.StringUtils.YEAR;
 
 
 public class SmartCalendarPlugin extends MessageOnTapPlugin {
@@ -38,7 +40,7 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
 
 
     private ParseTree tree1,tree2;
-    String EventTime1, EventTime2;
+    Long EventTime2;
 
     // init the tags
     Tag tag_I = new Tag("TAG_I", new HashSet<>(Collections.singletonList("I")));
@@ -139,7 +141,7 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
         if (params.get(Session.TRIGGER_SOURCE).equals("calendar_trigger_three")||
                 params.get(Session.TRIGGER_SOURCE).equals("calendar_trigger_four")){
             tree2 = (ParseTree)params.get(Graph.SYNTAX_TREE);
-            EventTime2 = params.get(CURRENT_MESSAGE_EMBEDDED_TIME);
+            EventTime2 = params.get(CURRENT_MESSAGE_EMBEDDED_TIME).get(0);
             params.put(BUBBLE_FIRST_LINE, "Add Calendar");
             params.put(BUBBLE_SECOND_LINE, "Event time:"+EventTime2);
             TidAdd1 = newTaskRequest(sid, MethodConstants.UI_SHOW, "BubbleShow", params);
@@ -168,7 +170,6 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
             try {
                 setLocation(params);
                 params.put(BUBBLE_FIRST_LINE, "Show Calendar");
-                params.put(BUBBLE_SECOND_LINE, "Event time:"+EventTime1);
                 TidShow2 = newTaskRequest(sid, MethodConstants.UI_SHOW, "Bubble", params);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -192,8 +193,10 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
 
 
         if (tid == TidAdd1){
-            ArrayList<Long> time = (ArrayList<Long>) params.get("time");
-            params.put("action:Add to calendar time", time.get(0));        //time 必须要精确到日期？
+            HashMap<String, Integer> date = getDate(EventTime2);           //transfer from Long to date
+            params.put("action:Add to calendar time", date.get(YEAR));
+            params.put("action:Add to calendar time", date.get(MONTH));
+            params.put("action:Add to calendar time", date.get(DAY));
             TidAdd2 = newTaskRequest(sid, MethodConstants.ACTION, "params", params);
         } else if (tid == TidAdd2){
             Log.e(TAG, "Ending session (triggerListAdd)");
@@ -289,9 +292,9 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
     private ParseTree AddRootEventName(ParseTree tree){
         for (ParseTree.Node node : tree.getNodeList){
             if (node.getParentId() == 0){
-                node.setParentId(3232);
+                node.setParentId(NAMEROOTID);
                 ParseTree.Node newNode = new ParseTree.Node();
-                newNode.setId(3232);
+                newNode.setId(NAMEROOTID);
                 newNode.setParentId(0);
                 Set<Integer> set = new HashSet<>();
                 set.add(node.getId());
@@ -305,9 +308,9 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
     private ParseTree AddRootLocation(ParseTree tree){
         for (ParseTree.Node node : tree.getNodeList){
             if (node.getParentId() == 0){
-                node.setParentId(213123);
+                node.setParentId(LOCATIONROOTID);
                 ParseTree.Node newNode = new ParseTree.Node();
-                newNode.setId(213123);
+                newNode.setId(LOCATIONROOTID);
                 newNode.setParentId(0);
                 Set<Integer> set = new HashSet<>();
                 set.add(node.getId());
@@ -339,6 +342,17 @@ public class SmartCalendarPlugin extends MessageOnTapPlugin {
             }
         }
     }
+
+    private HashMap<String, Integer> getDate(Long time){
+        Calendar beginT = Calendar.getInstance();
+        beginT.setTimeInMillis(time);
+        HashMap<String, Integer> date = new HashMap<>();
+        date.put(YEAR, beginT.get(Calendar.YEAR));
+        date.put(MONTH, beginT.get(Calendar.MONTH));
+        date.put(DAY, beginT.get(Calendar.MONTH));
+        return date;
+    }
+
 
 }
 
