7955bebc7fc0cc4ebba3e0c1fe8850e87d6cee23
JiaYeMao <610275388@qq.com>
2017-08-10 16:29:24 -0400

    add contact list to GoogleDocPlugin

---
+++ googledocsplugin/src/main/java/edu/cmu/chimps/googledocsplugin/ContactStorage.java
@@ -0,0 +1,26 @@
+package edu.cmu.chimps.googledocsplugin;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContactStorage {
+    public static final String POSITION = "send_contacts_position";
+    public static final String STORAGE = "save_contacts_file";
+    public static final String ALLSELECTSTORAGE = "save_contacts_file";
+
+    public static void storeSendUsers(Context context, Set<String>set, String filename){
+        SharedPreferences.Editor editor = context.getSharedPreferences(filename, context.MODE_PRIVATE).edit();
+        editor.putStringSet(POSITION, set);
+        editor.apply();
+    }
+
+    public static Set<String> getContacts(Context context, String filename){
+        SharedPreferences pref = context.getSharedPreferences(filename, context.MODE_PRIVATE);
+        return pref.getStringSet(POSITION, new HashSet<String>());
+    }
+
+
+}
